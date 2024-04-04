90c5a9b3bf27414516d338025a4edc5ea8ba4bb7
Nraihpuael <nahuelripa@hotmail.com>
2024-04-04 19:04:12 -0400

    filtro y notificacion

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
-import { NotificationService } from './notification.service';
+//import { NotificationService } from './notification.service';
 
 @Module({
   controllers: [NotificationController],
-  providers: [NotificationService]
+  providers: []
 })
 export class NotificationModule {}
