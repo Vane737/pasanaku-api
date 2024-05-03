f46f9668f3820252ea3ae4e0385c392ae5bd4774
Nraihpuael <nahuelripa@hotmail.com>
2024-05-02 22:58:01 -0400

    bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,24 +37,21 @@ import { TransferenciaModule } from './transferencia/transferencia.module';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        //url: configService.get('DATABASE_URL'),
-        host: process.env.POSTGRES_HOST,
-        port: +process.env.POSTGRES_PORT,
-        database: process.env.POSTGRES_DATABASE,
-        username: process.env.POSTGRES_USERNAME,
-        password: process.env.POSTGRES_PASSWORD,
+        url: configService.get('DATABASE_URL'),
+      // host: process.env.POSTGRES_HOST,
+      // port: +process.env.POSTGRES_PORT,
+      // database: process.env.POSTGRES_DATABASE,
+      // username: process.env.POSTGRES_USERNAME,
+      // password: process.env.POSTGRES_PASSWORD,
         entities: ['dist/src/**/*.entity{.ts,.js}'],
         autoLoadEntities: true, // Carga automaticamente las entidades
         synchronize: true,  // Realiza las migraciones automaticamente
-        //ssl: false,
-        /*
         ssl: true,
         extra: {
           ssl: {
             rejectUnauthorized: false,
           },
         }
-        */
       }),
      }),
     JugadoresModule,
